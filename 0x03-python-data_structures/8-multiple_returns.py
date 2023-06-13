7bbea3c54a96a0f55798a702b461d5628404f77e
root <root@DESKTOP-9NVBJJL>
2023-06-13 03:43:35 +0100

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,5 +3,5 @@ def multiple_returns(sentence):
     if len(sentence) == 0:
         t = len(sentence), None
         return t
-     t = len(sentence), sentence[0]
-     return t
+    t = len(sentence), sentence[0]
+    return t
